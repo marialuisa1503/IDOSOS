7ca435e8f31cceb78889ee056efff995e474cdc4
marialuisa1503 <maria.claudino.costa@escola.pr.gov.br>
2026-06-25 09:11:30 -0300

    25/06

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function mudarTamanhoTexto(modificador) {
     // Multiplica o modificador por 2 para aumentar ou diminuir de 2 em 2 pixels
     tamanhoFonteAtual = tamanhoFonteAtual + (modificador * 2);
     
-    // Limita o tamanho da fonte para não ficar nem minúsculo, nem gigante demais
+    // Limita o tamanho da fonte para nao ficar nem minusculo, nem gigante demais
     if (tamanhoFonteAtual < 18) {
         tamanhoFonteAtual = 18;
     } else if (tamanhoFonteAtual > 34) {
         tamanhoFonteAtual = 34;
     }
     
-    // Aplica o novo tamanho de fonte ao corpo inteiro da página (body)
+    // Aplica o novo tamanho de fonte ao corpo inteiro da pagina (body)
     document.body.style.fontSize = tamanhoFonteAtual + "px";
 }
